66e05840c036627ccb15cd08590607361b4b09d8
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2018-04-11 18:55:24 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import './components/Header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Sobre CSS
+      </div>
+    );
+  }
+}
+
+export default App;
